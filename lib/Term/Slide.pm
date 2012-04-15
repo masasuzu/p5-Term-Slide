fd2b0582a6512e05ae662f6911a1f40d4f0471ec
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2012-04-16 00:14:58 +0900

    move to next page and previous page

--- lib/Term/Slide.pm
+++ lib/Term/Slide.pm
@@ -1,7 +1,81 @@
 package Term::Slide;
 use strict;
 use warnings;
+use 5.14.2;
 our $VERSION = '0.01';
+
+use Term::Screen;
+
+my $slides = [
+    +{
+        content => [
+            +{ content => 'ほげ', },
+            +{ content => 'ほげ', },
+            +{ content => "\e[33mほげ\e[0m", wait => 1 },
+            +{ content => 'ほげ', },
+        ],
+    },
+    +{
+        content => [
+            +{ content => 'ほげ', },
+            +{ content => 'ほほげ', },
+        ],
+    },
+    +{
+        content => [
+            +{ content => 'ほげ', },
+            +{ content => 'ほほほげ', },
+            +{ content => 'ほげ', },
+            +{ content => 'ほげ', },
+        ],
+    },
+];
+
+sub new {
+    my ($class, $parser_class, $file) = @_;
+
+    my $parser = $parser_class->new($file);
+    return bless +{
+        __parser => $parser,
+    }, $class;
+
+}
+
+sub show {
+    my ($self) = @_;
+    my $screen    = Term::Screen->new;
+
+    my $rows    =  $screen->rows;
+    my $columns = $screen->cols;
+    #my $slides = $self->paser->data;
+
+    $screen->clrscr;
+
+    my $page_num = scalar @{ $slides };
+    my $page_index = 0;
+    while (0 <= $page_index && $page_index < $page_num) {
+
+        my $line_index = 0;
+        for my $line (@{ $slides->[$page_index]->{content} }) {
+            $screen->at($line_index)->puts($line->{content});
+            sleep 1 if $line->{wait};
+            $line_index++;
+        }
+        while ( my $c = $screen->at($rows, $columns)->getch ) {
+            if ( $c eq 'n' ) {
+                $page_index++;
+                last;
+            }
+            elsif ( $c eq 'p' ) {
+                $page_index--;
+                last;
+            }
+        }
+
+        $screen->clrscr;
+    }
+
+}
 
 1;
 __END__
